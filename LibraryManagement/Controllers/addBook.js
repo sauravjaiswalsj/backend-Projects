a47a7821816dc67ca595d0a5d12c0a6dd442c13f
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2023-01-12 01:35:49 +0530

    :added Add Book Route

--- LibraryManagement/Controllers/addBook.js
+++ LibraryManagement/Controllers/addBook.js
@@ -2,19 +2,25 @@ const Book = require('../models/BookModel');
 
 const addBook = async(req,res)=>{
     try{
-        const {name,author} = req.body;
+        const {title,author} = req.body;
 
         const isExist = await Book.findAll({
             where:{
-                name : name,
+                title : title,
                 author : author
             }
         });
-
-        if(isExist){
-            res.status(302)
+        console.log(isExist)
+        if(isExist.length !== 0){
+            return res.status(200).send(`Book : ${title} already exist`);
         }
-
+        try{
+            await Book.create({title,author});
+            return res.status(200).send(`${title} : ${author} added successfully to the library.`);
+        }catch(err){
+            console.log(`Unable to add Books: ${err}`);
+            res.status(500).send('Unable to add Books');
+        }
     }catch(err){
         console.log(`Error:${err}`);
     }
--- LibraryManagement/route/BookRoutes.js
+++ LibraryManagement/route/BookRoutes.js
@@ -4,13 +4,14 @@ const router = express.Router();
 
 //GetControllers
 const home = require('../Controllers/home');
+const addBook = require('../Controllers/addBook');
 
 createDb.sync().then(()=>{
     console.log(`Db is running`);
 });
 router.get('/',home);
 
-//router.post('/newBook',newBook);
+router.post('/add',addBook);
 
 //router.get('/searchBook',searchBook);
 
