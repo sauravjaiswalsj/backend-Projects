17fc37e0182e4a2e8f1dad6f8ab14814c77fe8d4
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2023-01-22 01:25:25 +0530

    added base route

--- BookStore/route/Store.js
+++ BookStore/route/Store.js
@@ -1,2 +1,22 @@
 //require the dependencies
-const express = 
+const express = require('express');
+const createDb = require('../config/db');
+
+//GetControllers
+
+//Define router
+const router = express.Router();
+
+//createSync in db
+
+createDb.sync().then(()=>console.log(`Db is running`));
+
+//router paths
+
+router.get('/books',);
+router.get('/books/:id',);
+router.post('/books/add',);
+router.put('/books/:id',);
+router.delete('/books/:id');
+
+module.exports = router;
