089079d7aab73be31d29f73504c426e9da080200
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2023-06-13 01:43:42 +0530

    complete

--- AuthenticationSystem/config/db.js
+++ AuthenticationSystem/config/db.js
@@ -6,20 +6,9 @@ const user = process.env.user;
 const password = process.env.password;
 
 //* Instantiates sequelize with the name of database, username, password and configuration options
-const createDb = new Sequelize("test-db",user,password,{
+const createDb = new Sequelize("test-db", user, password, {
     dialect: "sqlite",
-    host:"./config/db.sqlite"
-});
-
-const sequelize = createDb;
-
-//authenticate the db
-sequelize.authenticate()
-    .then(()=>{
-        console.log(`Authenticated to DB successfully.`);
-    })
-    .catch(err=>{
-        console.log(`Error occured during db connection: ${err}`);
+    host: "./config/db.sqlite"
 });
 
 module.exports = createDb;
