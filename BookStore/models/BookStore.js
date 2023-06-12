ddb5ecc886ef5a61a4baaf2ee2633e35fc16fe29
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2023-06-13 01:46:04 +0530

    complete

--- BookStore/models/BookStore.js
+++ BookStore/models/BookStore.js
@@ -4,6 +4,21 @@ const { Model, DataTypes } = require('sequelize');
 class BookStore extends Model { };
 
 BookStore.init({
+    id: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        primaryKey: true,
+        validate: {
+            notEmpty: true,
+        },
+        indexes: [
+            {
+                unique: true,
+                field: ['id']
+            }
+        ]
+    },
     name: {
         type: DataTypes.STRING,
     },
@@ -16,8 +31,9 @@ BookStore.init({
     dateOfRelease: { type: DataTypes.STRING, },
     bookImage: { type: DataTypes.STRING },
     rating: { type: DataTypes.NUMBER },
-    price: { type: DataTypes.NUMBER },
-}, { sequelize, modelName: "BookStore" }
+    price: { type: DataTypes.DECIMAL },
+
+}, { sequelize, createdAt: false, modelName: "BookStore" }
 );
 
 module.exports = BookStore;
