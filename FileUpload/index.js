490c3f36b5ff162b095ca6ffd043c52214b934d4
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2023-06-22 02:10:42 +0530

    added server

---
+++ FileUpload/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port http://localhost:${PORT}/`);
+});
